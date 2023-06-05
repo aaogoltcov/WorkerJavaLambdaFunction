7b4681c69efc93fd27a323ea64a5798a3c833a7e
Aleksei Ogoltcov <aaogoltcov@mai.ru>
2023-06-06 00:42:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        OnTaskDoneListener taskListener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(taskListener, errorListener);
+        worker.start();
+    }
+}
